1a18faaf204940dd665bc1d9513f40b6ae2e87af
Jisoo Kim <62425277+indexkim@users.noreply.github.com>
2021-07-18 21:29:57 +0900

    Create face_detect_move.py

---
+++ 2. ML training/face_detect_move.py
@@ -0,0 +1,15 @@
+import cv2
+import shutil
+
+
+def face_detect_move(yyyymmdd, path, folder, file, jpg_path):
+    refine_path = 'X:/localUser/TrainingData/Refinement/refine_pass/'+yyyymmdd+'_정제완료'
+    face_path = 'X:/localUser/TrainingData/Refinement/refine_face'
+    jpg_path = path+'/'+folder+'/'+file
+    image = cv2.imread(jpg_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier(
+        'haarcascades/haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(gray, 1.2, 5)
+    if len(faces) > 0:
+        shutil.move(refine_path+'/'+folder, face_path+'/'+folder+'_f')
